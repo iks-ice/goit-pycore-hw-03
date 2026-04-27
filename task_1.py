916771ef6fec8cd8569b1ac019f0ed15ce43ce7f
Oleh Parshyn <oleh.parshyn@monobank.ua>
2026-04-27 10:59:39 +0300

    refactoring && added call examples

--- task_1.py
+++ task_1.py
@@ -1,14 +1,23 @@
 from datetime import datetime
 
 def get_days_from_today(date = None):
+
+    if date is None:
+        return None
+
     try:
-        if date is None:
-            date = datetime.now().strftime("%Y-%m-%d")
-        today = datetime.today().date()
         target_date = datetime.strptime(date, "%Y-%m-%d").date()
-        print(target_date, today)
-        return (today - target_date).days
     except Exception as e:
         print(e)
         return None
+        
+    today = datetime.today().date()
+
+    return (today - target_date).days
     
+print(get_days_from_today("2026-04-27"))
+print(get_days_from_today("2026-05-27"))
+print(get_days_from_today("2025-05-27"))
+print(get_days_from_today())
+print(get_days_from_today(5))
+print(get_days_from_today("25-05-27"))
