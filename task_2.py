819ef3340ad08a88247fa4ac36a02454990a28ec
Oleh Parshyn <oleh.parshyn@monobank.ua>
2026-04-27 11:00:09 +0300

    refactoring

--- task_2.py
+++ task_2.py
@@ -1,23 +1,29 @@
 import random
 
 def get_numbers_ticket(min, max, quantity):
+    
     try:
         min = int(min)
         max = int(max)
         quantity = int(quantity)
-
-        if min < 1 or max > 1000 or quantity > max - min + 1:
-            return []
-
-        min_max_range = [number for number in range(min, max + 1)]
-        random_sequence = random.sample(min_max_range, quantity)
-        random_sequence.sort()
-
-        return random_sequence
-    
     except Exception as e:
         print(e)
         return []
 
-print(get_numbers_ticket(1, 2, 2))
+    if min < 1 or max > 1000 or quantity > max - min + 1:
+        return []
+
+    min_max_range = [number for number in range(min, max + 1)]
+    random_sequence = random.sample(min_max_range, quantity)
+    random_sequence.sort()
+
+    return random_sequence
+    
+    
+
+print(get_numbers_ticket(100, 200, 10))
+print(get_numbers_ticket(1, 10, 11))
+print(get_numbers_ticket(10, 1, 11))
+print(get_numbers_ticket(10, 10, 10))
+print(get_numbers_ticket(1, 10, 0))
         
